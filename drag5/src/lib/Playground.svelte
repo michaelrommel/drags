b747528c4db5ce1a0a2405dd0fef670e058a5d62
Michael Rommel <rommel@layer-7.net>
2025-05-27 09:11:46 +0200

    feat: proper window controls

--- drag5/src/lib/Playground.svelte
+++ drag5/src/lib/Playground.svelte
@@ -10,11 +10,11 @@
 	let fabric;
 	let center = $state([0, 0]);
 	let zoom = $state(1.0);
-	let terminalWindows = [
-		{ id: 1, x: 100, y: 20, rows: 24, cols: 80 },
-		{ id: 2, x: 150, y: 80, rows: 24, cols: 80 },
-		{ id: 3, x: 200, y: 40, rows: 24, cols: 80 },
-	];
+	let terminalWindows = $state([
+		{ id: 1, z: 1, x: 100, y: 20, rows: 24, cols: 80 },
+		{ id: 2, z: 3, x: 150, y: 80, rows: 24, cols: 80 },
+		{ id: 3, z: 2, x: 200, y: 40, rows: 24, cols: 80 },
+	]);
 
 	onMount(() => {
 		fabric = new FabricHandler({ fabricEl: fabricElement });
@@ -22,33 +22,60 @@
 			center = fabric.center;
 			zoom = fabric.zoom;
 		});
+
+		$effect(() => {
+			console.log(
+				"effect fabric: " +
+					JSON.stringify($state.snapshot(terminalWindows)),
+			);
+		});
 	});
+
+	const focusWindow = (id) => {
+		let num_of_windows = terminalWindows.length;
+		let order = num_of_windows - 1;
+		terminalWindows = terminalWindows
+			.sort((a, b) => {
+				return a.z == b.z ? 0 : a.z > b.z ? -1 : 1;
+			})
+			.map((win) => {
+				if (win.id == id) {
+					win.z = num_of_windows;
+				} else {
+					win.z = order;
+					order = order - 1;
+				}
+				return win;
+			});
+	};
 
 	// function listen(node, { name, handler }) {
 	// 	node.addEventListener(name, handler);
 	// 	return { destroy: () => node.removeEventListener(name, handler) };
 	// }
-	// style:background-image="radial-gradient(#333 {zoom}px, transparent 0)"
 </script>
 
 <div
-	style:position="absolute"
-	style:inset="0px"
-	style:z-index="-10"
-	style:background-image="url('leaves.jpg')"
-	style:background-size="{650 * zoom}px {650 * zoom}px"
-	style:background-repeat="no-repeat"
-	style:background-position="{-zoom * center[0]}px {-zoom * center[1]}px"
+	class="absolute inset-0 -z-10 bg-[#212121]"
+	style:background-image="radial-gradient(#404040 {1.5 * zoom}px, transparent
+	0), radial-gradient(#a00000 {5 * zoom}px, transparent 0)"
+	style:background-size="{24 * zoom}px {24 * zoom}px, {24 * zoom}px {24 *
+		zoom}px"
+	style:background-repeat="repeat, no-repeat"
+	style:background-position="{-zoom * (center[0] + 12)}px {-zoom *
+		(center[1] + 12)}px, {-zoom * (center[0] + 12)}px {-zoom *
+		(center[1] + 12)}px"
 ></div>
 <div
-	style:position="absolute"
-	style:inset="0px"
-	style:overflow="hidden"
-	style:background-color="rgba(1,1,1,0.5)"
-	style:touch-action="none"
+	class="absolute inset-0 overflow-hidden touch-none"
 	bind:this={fabricElement}
 >
-	{#each terminalWindows as terminalWindow}
-		<TermWindow {center} {zoom} {terminalWindow} />
+	{#each terminalWindows as terminalWindow, i (terminalWindow.id)}
+		<TermWindow
+			{center}
+			{zoom}
+			bind:terminalWindow={terminalWindows[i]}
+			{focusWindow}
+		/>
 	{/each}
 </div>
--- drag5/src/lib/TermWindow.svelte
+++ drag5/src/lib/TermWindow.svelte
@@ -11,7 +11,19 @@
 	const OFFSET_TOP_CSS = "0px";
 	const OFFSET_TRANSFORM_ORIGIN_CSS = `calc(-1 * ${OFFSET_LEFT_CSS}) calc(-1 * ${OFFSET_TOP_CSS})`;
 
-	/** Returns the mouse position in infinite grid coordinates, offset transformations and zoom. */
+	let { center, zoom, terminalWindow = $bindable(), focusWindow } = $props();
+	// let terminalWindow = { x: 0, y: 0, rows: 24, cols: 80 };
+
+	let windowElement;
+	let terminalElement;
+	let isMoving = -1; // are we moving
+	let movingOrigin = [0, 0]; // Coordinates of mouse at origin when drag started.
+	let movingIsDone = false; // Moving finished but hasn't been acknowledged.
+	let tweenie = $state();
+	let immediate = true;
+
+	// Returns the mouse position in infinite grid coordinates,
+	// offset transformations and zoom.
 	function normalizePosition(event) {
 		// const [ox, oy] = getConstantOffset();
 		let ox = 0;
@@ -22,23 +34,21 @@
 		];
 	}
 
-	let { center, zoom, terminalWindow } = $props();
-	// let terminalWindow = { x: 0, y: 0, rows: 24, cols: 80 };
-
-	let terminalElement;
-	let winSize = $state(terminalWindow); // New [x, y] position of the dragged terminal.
-	let isMoving = -1; // are we moving
-	let movingOrigin = [0, 0]; // Coordinates of mouse at origin when drag started.
-	let movingIsDone = false; // Moving finished but hasn't been acknowledged.
-	let tweenie = $state();
-	let immediate = true;
-
 	onMount(() => {
+		console.log(
+			"onMount: " + JSON.stringify($state.snapshot(terminalWindow)),
+		);
+		$effect(() => {
+			console.log(
+				"effect child: " +
+					JSON.stringify($state.snapshot(terminalWindow)),
+			);
+		});
 		function handleMouse(event) {
 			if (isMoving !== -1 && !movingIsDone) {
 				const [x, y] = normalizePosition(event);
-				winSize = {
-					...winSize,
+				terminalWindow = {
+					...terminalWindow,
 					x: Math.round(x - movingOrigin[0]),
 					y: Math.round(y - movingOrigin[1]),
 				};
@@ -54,21 +64,22 @@
 		function handlePointerDown(event) {
 			const [x, y] = normalizePosition(event);
 			isMoving = 1;
-			movingOrigin = [x - winSize.x, y - winSize.y];
+			movingOrigin = [x - terminalWindow.x, y - terminalWindow.y];
 			movingIsDone = false;
 			immediate = false;
+			focusWindow(terminalWindow.id);
 			event.stopPropagation();
 		}
 
 		on(window, "mousemove", handleMouse);
 		on(window, "mouseup", handleMouseEnd);
-		on(terminalElement, "pointerdown", handlePointerDown);
+		on(windowElement, "pointerdown", handlePointerDown);
 	});
 
 	tweenie = Tween.of(
 		() => {
 			return {
-				winSize: { x: 0, y: 0, rows: 24, cols: 80 },
+				winSize: { x: 0, y: 0, z: 0, rows: 24, cols: 80 },
 				center: [0, 0],
 				zoom: 1.0,
 			};
@@ -82,7 +93,7 @@
 	$effect(() => {
 		tweenie.set(
 			{
-				winSize,
+				winSize: terminalWindow,
 				center,
 				zoom,
 			},
@@ -94,7 +105,18 @@
 
 	const sl = (node) => {
 		$effect(() => {
+			console.log(
+				"ef slide child: " +
+					JSON.stringify(
+						$state.snapshot(tweenie?.current.winSize?.id),
+					) +
+					" => " +
+					JSON.stringify(
+						$state.snapshot(tweenie?.current.winSize?.x),
+					),
+			);
 			node.style.transform = `scale(${(tweenie?.current.zoom * 100).toFixed(3)}%) translate3d(${tweenie?.current.winSize?.x - tweenie?.current.center?.[0]}px, ${tweenie?.current.winSize?.y - tweenie?.current.center?.[1]}px, 0)`;
+			node.style["z-index"] = tweenie?.current.winSize?.z;
 		});
 	};
 	// function listen(node, { name, handler }) {
@@ -105,29 +127,53 @@
 </script>
 
 <div
-	id="wrapper"
-	style:position="absolute"
-	style:background-color="red"
-	style:padding="3rem"
+	class="absolute bg-gray-900 border rounded-2xl"
 	style:left={OFFSET_LEFT_CSS}
 	style:top={OFFSET_TOP_CSS}
 	style:transform-origin={OFFSET_TRANSFORM_ORIGIN_CSS}
 	transition:fade|local
 	use:sl
+	bind:this={windowElement}
 >
+	<div class="flex select-none">
+		<div class="flex-1 flex items-center px-3">
+			<div class="flex space-x-2 text-transparent hover:text-black/75">
+				<button
+					class="bg-red-500 w-3 h-3 rounded-full"
+					aria-label="Close"
+				></button>
+				<button
+					class="bg-yellow-500 w-3 h-3 rounded-full"
+					aria-label="Shrink"
+				></button>
+				<button
+					class="bg-green-500 w-3 h-3 rounded-full"
+					aria-label="Expand"
+				></button>
+			</div>
+		</div>
+		<div
+			class="p-2 text-sm text-zinc-300 text-center font-medium overflow-hidden whitespace-nowrap text-ellipsis w-0 flex-grow-[4]"
+		>
+			Remote Terminal
+		</div>
+		<div class="flex-1"></div>
+	</div>
+
 	<div
-		style:background-color="green"
-		style:padding="2rem"
+		class="p-2 text-slate-100 bg-gray-800 rounded-b-2xl"
 		bind:this={terminalElement}
 	>
-		This is version 5.
+		Windows with Svelte 5.<br /><br />
 		<pre>
+id={tweenie.current.winSize.id}
 x={tweenie.current.winSize.x.toFixed(3)}
 y={tweenie.current.winSize.y.toFixed(3)}
+z={tweenie.current.winSize.z.toFixed(3)}
 center=[ {tweenie.current.center[0].toFixed(
 				0,
 			)}, {tweenie.current.center[1].toFixed(0)} ]
 zoom={tweenie.current.zoom}
-			</pre>
+		</pre>
 	</div>
 </div>
